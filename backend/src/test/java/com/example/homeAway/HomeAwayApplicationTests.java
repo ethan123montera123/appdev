fa00623de9316c205a325d83eb57684302055d6e
Zenno Dominic P. Abellana <zennodominicabellana21@gmail.com>
2024-12-11 13:20:05 +0800

    added Backend

---
+++ backend/src/test/java/com/example/homeAway/HomeAwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homeAway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeAwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
